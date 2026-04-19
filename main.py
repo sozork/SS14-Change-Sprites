36da0624196807bac7b57467671e30dd0ffcb55b
sozork <vitchebrovskiy@mail.ru>
2026-04-19 18:15:10 +0300

    сделал показ спрайтов, теперь нужно их разместить и подправить

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import compress_decompress, sqlite3
 import os
 import base64
 import customtkinter as ctk
+from PIL import Image
+import io
 # sql запросы для ну что бы обойти ограничение на размер файлов(файл нельзя сменить т.к его размер не совпадает)
 remove_check = '''
 PRAGMA foreign_keys = false;
@@ -50,7 +52,7 @@ dont_show_path = []
 content_path = ''   # то где находится твоя база данных, не путать с путём до самого спрайта
 
 # Вставить с заменой файлы в баззу данных , берёт ид, путь до спрайта на который меняет
-def set_db_data(path, image_path):
+def set_db_data(content_path, path, image_path):
     connection = sqlite3.connect(content_path)
     cursor = connection.cursor() #  UPDATE Content SET Data = data_var WHERE Id = content_id
     content_id = cursor.execute('SELECT ContentId FROM ContentManifest WHERE path LIKE ?', [path])
@@ -69,7 +71,7 @@ def set_db_data(path, image_path):
     connection.commit()
     connection.close()
 # сохрание по пути до спрайта
-def save_current_image(path):
+def save_current_image(content_path, path):
     connection = sqlite3.connect(content_path)
     cursor = connection.cursor()
 
@@ -88,9 +90,7 @@ def save_current_image(path):
         f.write(data)
 
 # Выдаёт список из bool(ид внутри бд, пути, данные из бд и степени сжатия)
-def get_display_sprites(e):
-    global path, content_id
-    search_param = ''
+def get_display_sprites(content_path, search_param):
     # empty_folder("/temp")
     path = "%"+search_param+"%"
     connection = sqlite3.connect(content_path)
@@ -110,7 +110,7 @@ def get_display_sprites(e):
         instance_path = instance_path.fetchall()[-1][0]
 
         if compression != 0:
-            data = compress_decompress.decompress(x)
+            data = compress_decompress.decompress(data)
         pairs.append((i[0], instance_path, data, compression))
     
     return pairs
@@ -120,10 +120,69 @@ def get_display_sprites(e):
 class StartPage(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-        self.changespr_btn = ctk.CTkButton(self, text='Change Sprites')
+        # кнопки для перехода в различные фреймы приложения
+        self.changespr_btn = ctk.CTkButton(self, text='Change Sprites', command=lambda:(master.ChangesprPage.grid(**master.ChangesprPage.griddata), self.grid_forget())) # При нажатии Changespr  - показывает окно смены спрайтов и скрывает стартовое окно
         self.changespr_btn.grid(column = 0)
-        self.changespr_btn2 = ctk.CTkButton(self, text='Change Sprites')
-        self.changespr_btn2.grid(column = 1)
+        self.backups_btn = ctk.CTkButton(self, text='Backups')
+        self.backups_btn.grid(column = 1, row = 0)
+        self.packs_btn = ctk.CTkButton(self, text='Packs')
+        self.packs_btn.grid(column = 2, row = 0)
+    
+# Фрейм смены спрайтов
+class ChangesprPage(ctk.CTkFrame):
+    def __init__(self, master, griddata):
+        super().__init__(master)
+        # хэсшмап для пути до бд и пути до спрайта на который меняют
+        self.dict = {"content_path":"", "change_image_to_path": ""}
+        # свойства грида где он будет
+        self.griddata = griddata 
+        # кнопочка для выбора пути до контента
+        self.contentpath_btn = ctk.CTkButton(self, text='Content, path', command = lambda:self.get_file("content_path", [('DB', '*.db')])) # лямбду нужно ставить что бы функция не вызывалась при запуске
+        self.contentpath_btn.grid(column = 0)
+
+        # кнопочка выбора пути до картинки для смены
+        self.changeimgpath_btn = ctk.CTkButton(self, text='Image to change to, path', command = lambda:self.get_file("change_image_to_path", [("PNG","*.png"),("JPG","*.jpg"),("JPEG","*.jpeg")]))
+        self.changeimgpath_btn.grid(column = 1, row = 0)
+
+        # создания ввод парамтров поиска
+        self.entry = ctk.CTkEntry(master=self, width=400, height=50)
+        self.entry.grid(row=1, column=0, columnspan = 2)
+        self.entry.bind("<Return>", self.show_sprites)
+
+    # получаем файлы(фото для смены и путь до контента)
+    def get_file(self, key, filetypes):
+        self.dict[key] = ctk.filedialog.askopenfilename(filetypes=filetypes)
+
+    # Показ спрайтов
+    def show_sprites(self, e):
+        # создаём фрейм чисто для спрайтов
+        self.imageframe = ctk.CTkScrollableFrame(master=self, width=400)
+        self.imageframe.grid(column = 0, row = 3, columnspan = 2)
+        if self.dict["content_path"] == "":
+            pass
+        else:
+            pairs = get_display_sprites(self.dict["content_path"], self.entry.get())
+            print("pairs retrived, starting showing proccess")
+            n = len(pairs)
+            lenx = 2
+            leny = n//2
+            print(n, lenx, leny)
+            for row in range(leny):
+                for column in range(lenx):
+                    if not row*leny + column < n:
+                        break
+                    else:
+                        print('niguuuuur')
+                        pair = pairs[row*leny + column]
+                        print(row, column)
+                        content_id, path, data, compression = pair[0], pair[1], pair[2], pair[3]
+                        image = Image.open(io.BytesIO(data)) 
+                        self.imageframe.img_btn = ctk.CTkButton(self.imageframe, image=ctk.CTkImage(light_image=image, dark_image=image, size=(self.contentpath_btn.cget("width"), self.contentpath_btn.cget("width"))), text = "")
+                        self.imageframe.img_btn.grid(row = row+1, column = column+1)
+            
+
+            pass
+
 # само приложение
 class App(ctk.CTk):
     def __init__(self):
@@ -132,12 +191,15 @@ class App(ctk.CTk):
         self.title("SS14 Change Sprites")
         self.geometry("1000x840")
 
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)      # я не понял почему, но без этого не работает align гридов
         self.grid_columnconfigure(0, weight=1)
 
-        self.startframe = StartPage(self)
-        self.startframe.grid(row=0, column=0)
-        
+        self.ChangesprPage = ChangesprPage(self, {"column":0, "row":0}) # иницилизируеми сразу скрываем фрейм смены спрайтов
+        self.ChangesprPage.grid_forget() 
+
+        self.startframe = StartPage(self) # иницилизируем стартовый фрейм (всегда ставить последним, т.к в нём используются другие фреймы)
+        self.startframe.grid(row=0, column=0) # align в центре
+
 # запуск 
 if __name__ == "__main__":
     app = App()
